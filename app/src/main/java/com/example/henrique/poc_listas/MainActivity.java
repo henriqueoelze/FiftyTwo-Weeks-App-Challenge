fac1e59faf061f7326a7ed10df733ea65d45bd46
Oelze <oelze@ciandt.com>
2017-03-17 13:07:07 -0300

    Add click and setSelected on list

--- app/src/main/java/com/example/henrique/poc_listas/MainActivity.java
+++ app/src/main/java/com/example/henrique/poc_listas/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.henrique.poc_listas;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.style.BackgroundColorSpan;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.henrique.poc_listas.adapter.DayListAdapter;
 import com.example.henrique.poc_listas.domain.DayItem;
@@ -17,15 +22,28 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Calendar today;
+    private int positionToStart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        today = Calendar.getInstance();
         ListView list = (ListView) this.findViewById(R.id.dayList);
 
         DayListAdapter adapter = new DayListAdapter(this, R.layout.day_list, getItens());
         list.setAdapter(adapter);
+        list.setSelection(positionToStart - 1);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                view.setBackgroundColor(Color.GREEN);
+                Toast.makeText(getApplicationContext(), "Teste", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private List<DayItem> getItens() {
@@ -34,7 +52,15 @@ public class MainActivity extends AppCompatActivity {
         Calendar calendar = getFirstSundayOfYear();
         int year = calendar.get(Calendar.YEAR);
         int iteration = 1;
+
+        boolean searchForDayToStart = true;
         while(year == calendar.get(Calendar.YEAR)) {
+
+            if(searchForDayToStart && calendar.after(today)) {
+                positionToStart = iteration;
+                searchForDayToStart = false;
+            }
+
             int dayVaue = calendar.get(Calendar.DAY_OF_MONTH);
             int monthValue = calendar.get(Calendar.MONTH);
 
@@ -55,8 +81,6 @@ public class MainActivity extends AppCompatActivity {
     private Calendar getFirstSundayOfYear() {
         Calendar instance = Calendar.getInstance();
         instance.set(Calendar.DAY_OF_YEAR, 1);
-
-        Log.i("TESTE", instance.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.ENGLISH));
 
         while(instance.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
             instance.add(Calendar.DAY_OF_WEEK, 1);
