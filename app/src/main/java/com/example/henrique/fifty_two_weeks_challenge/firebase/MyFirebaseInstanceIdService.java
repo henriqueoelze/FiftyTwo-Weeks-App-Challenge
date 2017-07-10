ac96c3ae068dc10e1a0247cc9016e31df0db6928
Henrique Oelze <henriqueoelze@gmail.com>
2017-07-10 00:40:02 -0300

    Add started activity

--- app/src/main/java/com/example/henrique/fifty_two_weeks_challenge/firebase/MyFirebaseInstanceIdService.java
+++
@@ -1,9 +0,0 @@
-package com.example.henrique.fifty_two_weeks_challenge.firebase;
-
-import android.util.Log;
-
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
-
-public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
-}
